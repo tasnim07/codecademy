95f72d84f7fb134308fa1cb9f735cea0afa58046
Tasneem <tasnim07@gmail.com>
2015-08-07 02:31:54 +0530

    Battleship - A Game

---
+++ Battleship!/1.py
@@ -0,0 +1,5 @@
+board = []
+for i in range(0,5):
+    i = ["O"] * 5
+    board.append(i)
+print board    
---
+++ Battleship!/2.py
@@ -0,0 +1,9 @@
+board = []
+for i in range(0,5):
+    i = ["O"] * 5
+    board.append(i)
+def print_board(board):
+    for row in board:
+         
+        print row
+print print_board(board)        
